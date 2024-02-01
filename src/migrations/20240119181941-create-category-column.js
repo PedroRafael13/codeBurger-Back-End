0aeceda9f51c348f09116fc61f0e1f7175013ceb
PedroRafael13 <pedrorafaeldsn12@gmail.com>
2024-02-01 14:58:30 -0400

    rodando certo

---
+++ src/migrations/20240119181941-create-category-column.js
@@ -0,0 +1,13 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up : async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('categories', 'path' , {
+      type: Sequelize.STRING,
+     });
+     
+  },
+
+  down: async (queryInterface, Sequelize) => {
+     await queryInterface.removeColumn('categories', 'path'); 
+  }
+};
